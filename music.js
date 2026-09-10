fc7d9a83c58de6aacf21f09af58c570f4ee981a5
Twobbular <harry@clark.red>
2026-09-10 11:20:29 -0400

    made audio quiter

--- music.js
+++ music.js
@@ -1,13 +1,16 @@
 const audio = new Audio('bgm.mp3');
 
+// Set volume to 50%
+audio.volume = 0.5;
+
 audio.loop = true;
   
 // Try autoplay immediately (may fail silently)
 audio.play().catch(() => {
-console.log("Autoplay blocked, waiting for user interaction.");
+  console.log("Autoplay blocked, waiting for user interaction.");
 });
 
 // Play when user clicks anywhere
 document.body.addEventListener('click', () => {
-audio.play();
+  audio.play();
 }, { once: true });
